b1f6993750176af25ab4715897b1dfca6e7f1a8e
evivanderlinden <70517911+evivanderlinden@users.noreply.github.com>
2020-10-21 17:08:13 +0200

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,30 +1,80 @@
-
 // JavaScript Document
 
 //scroll to top micro interactie
-var scrollButton = document.querySelector("main section:nth-of-type(2) a");
-scrollButton.addEventListener("click", scrollToTop);
-
+var scrollButton2 = document.querySelector(".scroll-to-top");
+scrollButton2.addEventListener("click", scrollToTop);
 function scrollToTop() {
-    var position =
-        document.body.scrollTop || document.documentElement.scrollTop;
-    if (position) {
-        window.scrollBy(0, -Math.max(1, Math.floor(position / 10)));
-        scrollAnimation = setTimeout("scrollToTop()", 20);
-    } else clearTimeout(scrollAnimation);
+    document.body.scrollIntoView({behavior: 'smooth', block: 'start'});
 }
 
-//zijkant navigatie animatie
-var hamburger = document.querySelector("nav ul li:nth-of-type(4) a");
-hamburger.addEventListener("click", openNav);
+//hamburger menu uitklappen
+//code en hulp van CLifton van Henten
+(function() {
 
-var sluitHamburger = document.querySelector("div a:nth-of-type(1)");
-sluitHamburger.addEventListener("click", closeNav);
+  let hamburger = {
+    nav: document.querySelector('nav:nth-of-type(2)'),
+    navClick: document.querySelector('nav ul li:nth-of-type(4) a svg'),
 
-function openNav() {
-  document.getElementById("Sidenav").style.width = "100%";
-}
+    initialize() {
+      this.navClick.addEventListener('click', () => { this.toggle(); });
+    },
 
-function closeNav() {
-  document.getElementById("Sidenav").style.width = "0";
+    toggle() {
+      this.nav.classList.toggle('expanded');
+    },
+  };
+
+  hamburger.initialize();
+}());
+
+// //Footer uitklap menu
+var hetPark2 = document.getElementById("het_park_menu_footer");
+var overEfteling = document.getElementById("over_efteling_menu");
+var meerEfteling2 = document.getElementById("meer_efteling_menu_footer");
+var nieuwsbrief = document.getElementById("nieuwsbrief_menu");
+
+
+document.getElementById("het_park_btn_footer").addEventListener("click", function(){
+  uitklappen(hetPark2);
+});
+
+document.getElementById("over_efteling_btn").addEventListener("click", function(){
+  uitklappen(overEfteling);
+});
+
+document.getElementById("meer_efteling_btn_footer").addEventListener("click", function(){
+  uitklappen(meerEfteling2);
+});
+
+document.getElementById("nieuwsbrief_btn").addEventListener("click", function(){
+  uitklappen(nieuwsbrief);
+});
+
+//Navigatie uitklap menu
+var hetPark = document.getElementById("het_park_menu");
+var allSeizoen = document.getElementById("all_seizoen_menu");
+var meerEfteling = document.getElementById("meer_efteling_menu");
+
+
+document.getElementById("het_park_btn").addEventListener("click", function(){
+  uitklappen(hetPark);
+});
+
+document.getElementById("all_seizoen_btn").addEventListener("click", function(){
+  uitklappen(allSeizoen);
+});
+
+document.getElementById("meer_efteling_btn").addEventListener("click", function(){
+  uitklappen(meerEfteling);
+});
+
+function uitklappen(menu){
+
+  if (menu.style.display === "none"){
+    menu.style.display = "block";
+
+  }
+  else {
+    menu.style.display = "none";
+  }
 }
